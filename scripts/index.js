04d6df5d797d8ab33c27fe32182fff918f2dec9f
Pavel Rocket <pagg16pagg@gmail.com>
2021-08-17 21:49:29 +0300

    исправлены ошибки после 1 проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let closePopupButton = document.querySelector(".popup__button-close");
 let popup = document.querySelector(".popup");
-let openPopupButton = document.querySelector(".edit-button");
+let openPopupButton = document.querySelector(".button_edit");
 
 let formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector('input[name="name"]');
@@ -8,27 +8,28 @@ let jobInput = document.querySelector('input[name="job"]');
 let profileInfoTitle = document.querySelector(".profile-info__title");
 let profileInfoSubtitle = document.querySelector(".profile-info__subtitle");
 
-function togglePopup() {
+function addPopup() {
+  //функция открытия попапа и присвоения значений имени и вида деятельностии к текстовым полям инпут
+  popup.classList.add("popup_opened");
 
-  popup.classList.toggle("popup__opened");
-
-  if (popup.classList.contains("popup__opened")) {
-    jobInput.value = profileInfoSubtitle.textContent;
-    nameInput.value = profileInfoTitle.textContent;
-  }
+  jobInput.value = profileInfoSubtitle.textContent;
+  nameInput.value = profileInfoTitle.textContent;
 }
 
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
+function removePopup() {
+  //функция закрытия попапа
+  popup.classList.remove("popup_opened");
+}
 
 function formSubmitHandler(evt) {
-
   evt.preventDefault();
 
   profileInfoTitle.textContent = nameInput.value;
   profileInfoSubtitle.textContent = jobInput.value;
 
-  togglePopup();
+  removePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
+openPopupButton.addEventListener("click", addPopup);
+closePopupButton.addEventListener("click", removePopup);
